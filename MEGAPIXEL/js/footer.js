5ffca47c2edc69c59cbda32c9abaa64d32ad2a2f
Arjae Kristian B. Caparida <71628652+ArjaeCaparida@users.noreply.github.com>
2023-06-10 05:07:49 +0400

    removing data

--- MEGAPIXEL/js/footer.js
+++
@@ -1,4 +0,0 @@
-<script>
-    var currentYear = new Date().getFullYear();
-    document.getElementById("footer").innerHTML = "&copy; Megapixel - " + currentYear + "-" " <a href='https://github.com/ArjaeCaparida/Web-Apps---Social-Media-Clone--MegaPixel.git'> Arjae Kristian </a>";
-</script>
